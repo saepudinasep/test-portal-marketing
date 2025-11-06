ecaeff166aadb435fd62844016e499b8228c93d3
saepudinasep <saepudinasep2001@gmail.com>
2025-11-06 16:48:17 +0700

    [UPDATE] Merubah useSessionTimeout

--- frontend/src/hooks/useSessionTimeout.js
+++ frontend/src/hooks/useSessionTimeout.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export default function useSessionTimeout(timeout = 5 * 60 * 1000) {
   const navigate = useNavigate();
@@ -9,8 +10,17 @@ export default function useSessionTimeout(timeout = 5 * 60 * 1000) {
     clearTimeout(timerRef.current);
     timerRef.current = setTimeout(() => {
       sessionStorage.clear();
-      alert("Sesi Anda berakhir karena tidak ada aktivitas selama 5 menit.");
-      navigate("/");
+
+      Swal.fire({
+        title: "Sesi Berakhir",
+        text: "Sesi Anda berakhir karena tidak ada aktivitas selama 5 menit.",
+        icon: "warning",
+        confirmButtonText: "OK",
+        confirmButtonColor: "#3085d6",
+        allowOutsideClick: false,
+      }).then(() => {
+        navigate("/");
+      });
     }, timeout);
   };
 
