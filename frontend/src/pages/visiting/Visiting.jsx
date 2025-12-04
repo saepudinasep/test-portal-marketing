acbd9db0e6a6fcf69f7c5eeeba6c7dc5b41dc897
saepudinasep <saepudinasep2001@gmail.com>
2025-12-04 17:09:14 +0700

    [UPDATE] merubah script appscript visit terkait sumber data

--- frontend/src/pages/visiting/Visiting.jsx
+++ frontend/src/pages/visiting/Visiting.jsx
@@ -373,7 +373,7 @@ export default function Visiting() {
             });
 
             const response = await fetch(
-                "https://script.google.com/macros/s/AKfycbwKSaZitafhni66hF3FpSpWVKc9d27byVuV8Z-8HhG8RyIYFzShoiIlqEnW3sTnqm7o/exec",
+                "https://script.google.com/macros/s/AKfycbw8XaeprUMGs4WBkFm1_aX5ZUITbl-jgJ9-EsiFu8R0c6wWWcK2SUUmLGJ8Y29gUXKW/exec",
                 {
                     method: "POST",
                     body: JSON.stringify(payload),
