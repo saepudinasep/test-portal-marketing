ae8c5c37e379e34a7bb9ccf7006965cb614fc484
saepudinasep <saepudinasep2001@gmail.com>
2025-12-31 21:58:02 +0700

    [UPDATE] Menonaktifkan visit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -103,7 +103,7 @@ export default function App() {
 
         {/* Visiting Pages */}
         {/* <Route path="maintance" element={<Maintance user={userData} />} /> */}
-        <Route path="visiting" element={<Visiting user={userData} />} />
+        {/* <Route path="visiting" element={<Visiting user={userData} />} /> */}
         <Route path="rekrutMA" element={<RekrutMA user={userData} />} />
         <Route path="leads" element={<InputDatabase user={userData} />} />
       </Route>
